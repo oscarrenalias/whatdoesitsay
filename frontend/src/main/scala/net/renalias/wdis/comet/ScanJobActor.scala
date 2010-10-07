3ac4a193b1b2a64087f9c3052722a4198f6d1dda
Oscar Renalias <oscar@renalias.net>
2010-10-08 01:30:59 +0300

    Some changes and improvements

--- frontend/src/main/scala/net/renalias/wdis/comet/ScanJobActor.scala
+++ frontend/src/main/scala/net/renalias/wdis/comet/ScanJobActor.scala
@@ -25,57 +25,49 @@ class ScanJobActor extends CometActor with SimpleLogger {
 	
 	// to track whether the job has been completed or not
 	var jobComplete = false
-	
-	var jobId = S.param("documentId").openOr("")
-	//var jobId = "123"
+
+	// lifespan of the comet actor
+	override def lifespan = Full(5 minutes)	
 	
 	lazy val errorNotFound = Text("Job information not found")
+
+	// retrieves the job identifier
+	def jobId = name match {
+		case Empty => log.warning("Empty jobId for actor!"); "" 
+		case Full(x) => log.debug("Actor jobId: " + x); x
+	}
 
 	def render = {
 		ScanJob.find(By(ScanJob.jobId, jobId)) match {
 			case Full(job) if(job.status.is != ScanJobStatus.Completed) => {
 				log.debug("Job " + jobId + "isn't ready yet, setting up the actor...")
-				ScanJobMonitor ! AddJobListener(jobId, this)
 				Text("job: " + job.jobId.is + " - status: " + job.status.is)
 			}
 			case Full(job) if(job.status.is == ScanJobStatus.Completed) => {
-				log.debug("Job " + jobId + "is already completed, not need to set up the actor")
-				Text("job: " + job.jobId.is + " - status: " + job.status.is + "-text:" + job.text.is)
+				//log.debug("Job " + jobId + "is already completed, not need to set up the actor")
+				//Text("job: " + job.jobId.is + " - status: " + job.status.is + "-text:" + job.text.is)
+				<lift:embed what="/templates-hidden/job-data" />
 			}
 			case _ => errorNotFound
 		}
 	}
 	
 	override def localSetup = {
-		log.debug("Stating comet actor")		
-		
-		/*ScanJob.find(By(ScanJob.jobId, jobId)) match {
-			case Full(job) if(job.status.is != ScanJobStatus.Completed) => {
-				log.debug("Job " + jobId + "isn't ready yet, setting up the actor...")
-				ScanJobMonitor ! AddJobListener(jobId, this)
-				Text("job: " + job.jobId.is + " - status: " + job.status.is)
-				this ! ShutDown
-			}
-			case Full(job) if(job.status.is == ScanJobStatus.Completed) => {
-				log.debug("Job " + jobId + "is already completed, not need to set up the actor")
-				Text("job: " + job.jobId.is + " - status: " + job.status.is + "-text:" + job.text.is)
-			}
-			case _ => errorNotFound
-		}*/
+		log.debug("Starting comet actor: " + {jobId})		
 		ScanJobMonitor ! AddJobListener(jobId, this)
 	}
 	
 	override def localShutdown = {
-		log.debug("Shutting down comet actor")
-		ScanJobMonitor ! RemoveJobListener(jobId, this)
+		log.debug("Shutting down comet actor: " + {jobId})
+		ScanJobMonitor ! RemoveJobListener(jobId, this)		
 	}
 	
 	override def lowPriority = {
 		case JobCompleted(jobId) => {
 			log.debug("Job " + jobId + " notified as complete")
 			jobComplete = true
-			reRender(true)
-			this ! ShutDown
+			reRender(devMode)			
+			ScanJobMonitor ! RemoveJobListener(jobId, this)			
 		}
 		case _ => log.error("ScanJobActor got a message that did not understand")
 	}
--- frontend/src/main/scala/net/renalias/wdis/snippet/Document.scala
+++ frontend/src/main/scala/net/renalias/wdis/snippet/Document.scala
@@ -20,9 +20,9 @@ import net.renalias.wdis.io.FileHelper._
 import net.renalias.wdis.model._
 import net.renalias.wdis.config._
 
-class Document {
+class Document extends SimpleLogger {
 	
-	def showDocument: NodeSeq = {
+	def showDocument(xhtml: NodeSeq): NodeSeq = {
 		
 		lazy val errorNotFound = Text("The document could not be found")
 		
@@ -31,9 +31,15 @@ class Document {
 				// load the job from the db
 				ScanJob.find(By(ScanJob.jobId, docId)) match {
 					case Full(job) if(job.status.is != ScanJobStatus.Completed) => {
-						 <lift:comet type="ScanJobActor" />
+						log.debug("docId = " + docId)
+						<lift:comet type="ScanJobActor" name={docId}/>
 					}
-					case Full(job) if(job.status.is == ScanJobStatus.Completed) => Text("job: " + job.jobId.is + " - status: " + job.status.is + "-text:" + job.text.is)
+					case Full(job) if(job.status.is == ScanJobStatus.Completed) => {
+						//Text("job: " + job.jobId.is + " - status: " + job.status.is + "-text:" + job.text.is)
+						//<lift:embed what="/templates-hidden/job-data" />
+						bind("document", xhtml, "id" -> job.jobId.is, "status" -> job.status.is.toString, "text" -> job.text.is)
+						//bind("document", xhtml, "id" -> "job.jobId.is", "status" -> "job.status.is", "text" -> "job.text.is")
+					}
 					case _ => errorNotFound
 				}
 			}
