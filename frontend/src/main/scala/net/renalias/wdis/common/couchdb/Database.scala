9445c20c6ad9be8ee9b2441b460cf14120e59029
Oscar Renalias <oscar@renalias.net>
2010-11-06 17:05:05 +0200

    Replaced the Mapper database class with a CouchDB record class

---
+++ frontend/src/main/scala/net/renalias/wdis/common/couchdb/Database.scala
@@ -0,0 +1,11 @@
+package net.renalias.wdis.common.couchdb
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: oscar
+ * Date: Nov 5, 2010
+ * Time: 8:01:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class Database
---
+++ frontend/src/test/scala/net/renalias/wdis/common/model/ScanJobTest.scala
@@ -0,0 +1,11 @@
+package net.renalias.wdis.common.model
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: oscar
+ * Date: Nov 6, 2010
+ * Time: 12:48:08 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class ScanJobTest
