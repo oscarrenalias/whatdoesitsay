43b475243f8f1cac8cebecebf327392650e68ab7
Oscar Renalias <oscar@renalias.net>
2010-11-06 23:32:35 +0200

    Added a method to obtain the name of a file given a full file name (including extension)

--- frontend/src/main/scala/net/renalias/wdis/common/io/FileHelper.scala
+++ frontend/src/main/scala/net/renalias/wdis/common/io/FileHelper.scala
@@ -30,5 +30,12 @@ object FileHelper {
 			case x:Int if x==(fileName.length-1) => None
 			case x:Int => new Some(fileName.substring(x+1).toLowerCase)
 		}		
-	}	
+	}
+
+	def getFileName(fileName:String): String = {
+		fileName.lastIndexOf('.') match {
+			case -1 => fileName
+			case x:Int => fileName.substring(0, x)
+		}
+	}
 }
--- frontend/src/test/scala/net/renalias/wdis/common/io/FileHelperTest.scala
+++ frontend/src/test/scala/net/renalias/wdis/common/io/FileHelperTest.scala
@@ -23,4 +23,16 @@ class FileHelperTestSuite extends FunSuite with ShouldMatchers {
 		val f = new File("./src/test/resources/testfile.txt")
 		f.read("UTF-8") should equal ("file contents")
 	}
+
+	test("File extensions are returned correctly") {
+		FileHelper.getFileName("file.ext") should equal("file")
+	}
+
+	test("For files with multiple . in the filename, the extension is correctly returned") {
+		FileHelper.getFileName("file.ext1.ext2") should equal("file.ext1")
+	}
+
+	test("For files with no extension, the entire name is returned") {
+		FileHelper.getFileName("file") should equal("file")		
+	}
 }
