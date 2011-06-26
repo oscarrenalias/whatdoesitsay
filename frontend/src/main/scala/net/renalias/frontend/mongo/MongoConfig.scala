3576a357f95d153dd83bf4f887790b0585e558d2
Oscar Renalias <oscar@renalias.net>
2011-06-26 23:42:26 +0300

    Throw an exception if some of the properties are missing.

--- frontend/src/main/scala/net/renalias/frontend/mongo/MongoConfig.scala
+++ frontend/src/main/scala/net/renalias/frontend/mongo/MongoConfig.scala
@@ -3,11 +3,12 @@ package net.renalias.frontend.mongo
 import net.liftweb.util.Props
 import net.liftweb.mongodb.{DefaultMongoIdentifier, MongoDB}
 import com.mongodb.{Mongo, MongoOptions, ServerAddress}
+import net.renalias.frontend.config.PimpedProps
 
 object MongoConfig {
 	def init {
-		val srvr = new ServerAddress(Props.get("mongodb.host", "localhost"), Props.getInt("mongodb.port", 27017))
+		val srvr = new ServerAddress(PimpedProps.get_!("mongodb.host"), Props.getInt("mongodb.port", 27017))
 		val mo = new MongoOptions
-		MongoDB.defineDb(DefaultMongoIdentifier, new Mongo(srvr, mo), Props.get("mongodb.dbname", "test"))
+		MongoDB.defineDb(DefaultMongoIdentifier, new Mongo(srvr, mo), PimpedProps.get_!("mongodb.dbname"))
 	}
 }
